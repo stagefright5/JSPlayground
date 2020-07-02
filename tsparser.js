fa52b72f627e6c577ef190866d78a4b219d793d8
Sankrshan A Joshi <sankarshanaj@gmail.com>
2020-07-02 21:16:07 +0530

    refactor to tsparser to its own module

--- tsparser.js
+++ tsparser.js
@@ -1,27 +1,99 @@
+
+/*********** {START - Initializations, Definitions} ************/
 const ts = require('typescript');
-const { tscode } = require('./data');
 
-let code = process.argv[2] || tscode;
+/**
+ * 
+ * @param {import('typescript').ScriptTarget} target 
+ * @param {boolean} setParentNode 
+ */
+module.exports = function TSParser(target, setParentNode) {
 
-
-function getSourceFileObject(code) {
-	return ts.createSourceFile('temp.ts', code, ts.ScriptTarget.ES2019, true, ts.ScriptKind.TS);
-}
-
-function getMethodDeclarationKindNode(node) {
-	if (ts.isPropertyDeclaration(node)) {
-
-		/* TODO: @sankarshanaj - extract input/output params by following below steps
-			1. Start looping through node.decorators if present
-				1a. If yes, get the node.decorators[i]?.expression.expression.text
-				1b. If text is "Input" or "Output"
-					1bi. if decorator[i] has arguments get that text
-					1bj. Else get the node.name.text
-				1c. Push the got text value to Result.["Input" or "Output"] array 
-		*/
-		console.log('node:', node);
+	const FRO = { // FunctionResultObject
+		getIOPropertyNames: {}
 	}
-	ts.forEachChild(node, (n) => getMethodDeclarationKindNode(n));
-}
-const ast = getSourceFileObject(code);
-getMethodDeclarationKindNode(ast);
+	/**
+	 * @param {string} code
+	 */
+	function getSourceFileObject(code) {
+		return ts.createSourceFile('temp.ts', code, target, setParentNode, ts.ScriptKind.TS);
+	}
+
+	/**
+	 * 
+	 * @param {import('typescript').Node} node 
+	 */
+	function getIOPropertyNames(node) {
+		if (ts.isPropertyDeclaration(node)) {
+			const IOProperties = _getIOPropertyDecorators(node);
+			Object.keys(FRO.getIOPropertyNames).forEach(key => { // key = 'input', 'output'
+				const v = IOProperties[key].map(io => _getIODecoratorNames(io, node));
+				v.length && FRO.getIOPropertyNames[key].push(...v);
+			});
+		}
+		ts.forEachChild(node, getIOPropertyNames);
+	}
+
+	/**
+	 * @typedef {Object} result
+	 * @property {Array<import('typescript').Decorator>} inputs
+	 * @property {Array<import('typescript').Decorator>} outputs
+	 */
+	/**
+	 * @param {import('typescript').Node} node
+	 * @returns {result}
+	 */
+	function _getIOPropertyDecorators(node) {
+		/**
+		 * @type {result}
+		 */
+		const result = { inputs: [], outputs: [] };
+		node.decorators && node.decorators.forEach(decorator => {
+			const fullText = decorator.getFullText().trim();
+			if (/^@Input\(/.test(fullText)) {
+				result.inputs.push(decorator);
+			} else if (/^@Output\(/.test(fullText)) {
+				result.outputs.push(decorator);
+			}
+		});
+		return result;
+	}
+
+	/**
+	 * @param {import('typescript').Decorator} io 
+	 * @param {import('typescript').Node} node 
+	 */
+
+	function _getIODecoratorNames(io, node) {
+		return getDecoratorArgs(io)[0] || node.name.getText();
+	}
+
+	/**
+	 * @param {import('typescript').Decorator} decorator 
+	 * @returns {string[]}
+	 */
+	function getDecoratorArgs(decorator) {
+		/**
+		 * @type {import('typescript').CallExpression}
+		 */
+		const expr = decorator.expression;
+		return (expr && expr.arguments && expr.arguments.pos < expr.arguments.end) ? expr.arguments.map(a => (a.end > a.pos) && (a.getFullText().trim().replace(/\'|\"/g, ''))) : [''];
+	}
+
+	return {
+		getSource: getSourceFileObject,
+		/**
+		 * 
+		 * @param {import('typescript').Node} ast 
+		 */
+		getIOs: function (ast) {
+			FRO.getIOPropertyNames = { inputs: [], outputs: [] };
+			getIOPropertyNames(ast);
+			return FRO.getIOPropertyNames;
+		}
+	};
+};
+
+/*********** {END - Initializations, Definitions} ************/
+
+
